da84a288f89fa9408584efe127e440dc42f67152
indexzero <charlie.robbins@gmail.com>
2017-09-26 21:36:48 -0400

    [api refactor] Initial import of functionality from `winston@3` alpha.

---
+++ simple.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const util = require('util');
+const format = require('./format');
+
+/*
+ * function simple (opts)
+ * Returns a new instance of the simple format TransformStream
+ * which writes a simple representation of logs.
+ *
+ *    ${info.level}: ${info.message} ${JSON.stringify(info)}
+ */
+module.exports = format(function (info, opts) {
+  info.raw = util.format('%s: %s %j', info.level, info.message, info);
+  return info;
+});
---
+++ uncolorize.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const colors = require('colors/safe');
+const format = require('./format');
+
+/*
+ * function uncolorize (opts)
+ * Returns a new instance of the uncolorize format TransformStream
+ * which strips colors from `info` objects. This was previously
+ * exposed as the `stripColors` option to transports in `winston < 3.0.0`.
+ */
+module.exports = format(function (info, opts) {
+  info.level = colors.strip(info.level);
+  info.message = colors.strip(info.message);
+
+  if (info.raw) {
+    info.raw = colors.strip(info.raw);
+  }
+
+  return info;
+});
