754d61fd4d901f5bf8f52b6fa361ed4b6779a22b
crobbins <crobbins@godaddy.com>
2018-03-16 10:16:18 -0700

    [fix tiny] 2-space soft-tabs.

--- index.d.ts
+++ index.d.ts
@@ -5,9 +5,9 @@
 // TypeScript Version: 2.2
 
 export interface TransformableInfo {
-    level: string;
-    message: string;
-    [key: string]: any;
+  level: string;
+  message: string;
+  [key: string]: any;
 }
 
 export type TransformFunction = (info: TransformableInfo, opts?: any) => TransformableInfo | boolean;
@@ -15,18 +15,18 @@ export type Colors = { [key: string]: string | string[] }; // tslint:disable-lin
 export type FormatWrap = (opts?: any) => Format;
 
 export class Format {
-    constructor(opts?: object);
+  constructor(opts?: object);
 
-    options?: object;
-    transform: TransformFunction;
+  options?: object;
+  transform: TransformFunction;
 }
 
 export class Colorizer extends Format {
-    constructor(opts?: object);
+  constructor(opts?: object);
 
-    createColorize: (opts?: object) => Colorizer;
-    addColors: (colors: Colors) => Colors;
-    colorize: (level: string, message: string) => string;
+  createColorize: (opts?: object) => Colorizer;
+  addColors: (colors: Colors) => Colors;
+  colorize: (level: string, message: string) => string;
 }
 
 export function format(transform: TransformFunction): FormatWrap;
@@ -34,18 +34,18 @@ export function format(transform: TransformFunction): FormatWrap;
 export function levels(config: object): object;
 
 export namespace format {
-    function align(opts?: object): Format;
-    function cli(opts?: object): Format;
-    function colorize(opts?: object): Colorizer;
-    function combine(...formats: Format[]): Format;
-    function json(opts?: object): Format;
-    function label(opts?: object): Format;
-    function logstash(opts?: object): Format;
-    function padLevels(opts?: object): Format;
-    function prettyPrint(opts?: object): Format;
-    function printf(templateFunction: (info: TransformableInfo) => string): Format;
-    function simple(opts?: object): Format;
-    function splat(opts?: object): Format;
-    function timestamp(opts?: object): Format;
-    function uncolorize(opts?: object): Format;
+  function align(opts?: object): Format;
+  function cli(opts?: object): Format;
+  function colorize(opts?: object): Colorizer;
+  function combine(...formats: Format[]): Format;
+  function json(opts?: object): Format;
+  function label(opts?: object): Format;
+  function logstash(opts?: object): Format;
+  function padLevels(opts?: object): Format;
+  function prettyPrint(opts?: object): Format;
+  function printf(templateFunction: (info: TransformableInfo) => string): Format;
+  function simple(opts?: object): Format;
+  function splat(opts?: object): Format;
+  function timestamp(opts?: object): Format;
+  function uncolorize(opts?: object): Format;
 }
