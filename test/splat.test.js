825a7768b2527ab5d237ea5a9be77814f973dd1d
Charlie Robbins <charlie.robbins@gmail.com>
2018-06-12 04:11:31 -0400

    Migrate functionality from winston Logger to splat format. (#38)
    * [refactor api test] Migrate functionality from winston Logger to splat format.
    
    * [api] Support %c for colors in web scenarios: https://developers.google.com/web/tools/chrome-devtools/console/console-write

--- test/splat.test.js
+++ test/splat.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { SPLAT } = require('triple-beam');
 const assume = require('assume');
 const splat = require('../splat');
 const helpers = require('./helpers');
@@ -12,11 +13,16 @@ const helpers = require('./helpers');
 function assumeSplat(message, spread, expected) {
   return helpers.assumeFormatted(
     splat(),
-    { level: 'info', message, splat: spread },
+    { level: 'info', message, [SPLAT]: spread },
     info => {
       assume(info.level).is.a('string');
       assume(info.message).is.a('string');
-      assume(Array.isArray(info.splat)).true();
+      assume(Array.isArray(info[SPLAT])).true();
+
+      // Prefer any user-defined assertion (if provided).
+      if (typeof expected === 'function') {
+        return expected(info);
+      }
 
       assume(info.message).equals(expected);
     }
@@ -25,24 +31,38 @@ function assumeSplat(message, spread, expected) {
 
 describe('splat', () => {
   it('%s | string placeholder sets info.message', assumeSplat(
-    'alright %s', ['what'], 'alright what')
-  );
+    'alright %s', ['what'], 'alright what'
+  ));
 
   it('%d | number placeholder sets info.message', assumeSplat(
-    'test message %d', [123], 'test message 123')
-  );
+    'test message %d', [123], 'test message 123'
+  ));
 
   it('%j | json placeholder sets info.message', assumeSplat(
-    'test %j', [{ number: 123 }], 'test {"number":123}')
-  );
+    'test %j', [{ number: 123 }], 'test {"number":123}'
+  ));
 
   it('%% | escaped % sets info.message', assumeSplat(
-    'test %d%%', [100], 'test 100%')
-  );
+    'test %d%%', [100], 'test 100%'
+  ));
+
+  it('no % and no splat | returns the same info', assumeSplat(
+    'nothing to see here', [], 'nothing to see here'
+  ));
+
+  it('no % but some splat | returns the same info', assumeSplat(
+    'Look! No tokens!', ['ok'], info => {
+      assume(info.message).equals('Look! No tokens!');
+      assume(info.meta).equals(undefined);
+    }
+  ));
 
   it('Splat overflow (too many arguments) sets info.message', assumeSplat(
     '%s #%d, how are you %s',
     ['Hi', 42, 'feeling', { today: true }],
-    'Hi #42, how are you feeling { today: true }'
+    info => {
+      assume(info.message).equals('Hi #42, how are you feeling');
+      assume(info.meta).deep.equals({ today: true });
+    }
   ));
 });
