8939a0d47e93650b6691f1fd70ddd7330a7d1a5f
indexzero <charlie.robbins@gmail.com>
2019-01-04 16:50:47 -0500

    [wip dist api] 4:50pm. Press return.

--- errors.js
+++ errors.js
@@ -11,9 +11,21 @@ const { MESSAGE } = require('triple-beam');
  *
  * Optionally, the Error's `stack` property can also be appended to the `info` object.
  */
-module.exports = format((info, opts) => {
+module.exports = format((info, { stack }) => {
+  if (info instanceof Error) {
+    return Object.assign({}, info, {
+      message: info.message,
+      [MESSAGE]: info.message,
+      stack: stack ? info.stack : undefined
+    });
+  }
+
+  console.dir(info.message);
+  console.dir(info.stack);
+  console.dir(info instanceof Error);
   if (!(info.message instanceof Error)) return info;
 
+  console.dir('wtf');
   const err = info.message;
 
   info.message = err.message;
