4504228bf1bbaddcb37e7956115ed95c8adb7155
Charlie Robbins <charlie.robbins@gmail.com>
2019-01-07 22:26:22 -0800

    Address winston#1338 (updates from #59) (#69)
    * [wip dist api] 4:50pm. Press return.
    
    * [api wip] 5:00pm. Press return.
    
    * [fix tiny] Properly scope vars. Remove console debugging.
    
    * [fix] Handle custom error properties.
    
    * [test] 100% test coverage for updated errors format.
    
    * [doc] Update README.md

--- errors.js
+++ errors.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const format = require('./format');
-const { MESSAGE } = require('triple-beam');
+const { LEVEL, MESSAGE } = require('triple-beam');
 
 /*
  * function errors (info)
@@ -11,15 +11,29 @@ const { MESSAGE } = require('triple-beam');
  *
  * Optionally, the Error's `stack` property can also be appended to the `info` object.
  */
-module.exports = format((info, opts) => {
-  if (!(info.message instanceof Error)) return info;
+module.exports = format((einfo, { stack }) => {
+  if (einfo instanceof Error) {
+    const info = Object.assign({}, einfo, {
+      level: einfo.level,
+      [LEVEL]: einfo[LEVEL] || einfo.level,
+      message: einfo.message,
+      [MESSAGE]: einfo[MESSAGE] || einfo.message
+    });
 
-  const err = info.message;
+    if (stack) info.stack = einfo.stack;
+    return info;
+  }
 
-  info.message = err.message;
-  info[MESSAGE] = err.message;
+  if (!(einfo.message instanceof Error)) return einfo;
 
-  if (opts.stack) info.stack = err.stack;
+  // Assign all enumerable properties and the
+  // message property from the error provided.
+  Object.assign(einfo, einfo.message);
+  const err = einfo.message;
+  einfo.message = err.message;
+  einfo[MESSAGE] = err.message;
 
-  return info;
+  // Assign the stack if requested.
+  if (stack) einfo.stack = err.stack;
+  return einfo;
 });
--- test/errors.test.js
+++ test/errors.test.js
@@ -7,7 +7,30 @@ const { MESSAGE } = require('triple-beam');
 
 const err = new Error('whatever');
 
-describe('errors', () => {
+const errProps = new Error('another error');
+errProps.whatever = true;
+errProps.wut = 'some string';
+
+const errInfo = new Error('Already has level');
+errInfo.level = 'error';
+
+const errInfoProps = new Error('Level with props');
+errInfoProps.level = 'error';
+errInfoProps.whatever = true;
+errInfoProps.wut = 'some string';
+
+describe('errors()({ object })', () => {
+  it('errors() returns the original info', assumeFormatted(
+    errors(),
+    { level: 'info', message: 'whatever' },
+    (info) => {
+      assume(info.level).is.a('string');
+      assume(info.message).is.a('string');
+      assume(info.level).equals('info');
+      assume(info.message).equals('whatever');
+    }
+  ));
+
   it('errors() (default) sets info[MESSAGE]', assumeFormatted(
     errors(),
     { level: 'info', message: err },
@@ -20,7 +43,7 @@ describe('errors', () => {
     }
   ));
 
-  it('errors({ space: 2 }) sets info.stack', assumeFormatted(
+  it('errors({ stack: true }) sets info.stack', assumeFormatted(
     errors({ stack: true }),
     { level: 'info', message: err },
     (info) => {
@@ -28,8 +51,51 @@ describe('errors', () => {
       assume(info.message).is.a('string');
       assume(info.level).equals('info');
       assume(info.message).equals(err.message);
-      assume(info.stack).equals(err.stack);
       assume(info[MESSAGE]).equals(err.message);
+      assume(info.stack).equals(err.stack);
     }
+  ));
+
+  it('errors() sets custom error properties', assumeFormatted(
+    errors(),
+    { level: 'info', message: errProps },
+    (info) => {
+      assume(info.level).is.a('string');
+      assume(info.message).is.a('string');
+      assume(info.level).equals('info');
+      assume(info.message).equals(errProps.message);
+      assume(info[MESSAGE]).equals(errProps.message);
+      assume(info.whatever).true();
+      assume(info.wut).equals('some string');
+    }
+  ));
+});
+
+describe('errors()(Error)', () => {
+  it('errors() (default) sets info[MESSAGE]', assumeFormatted(
+    errors(),
+    errInfo,
+    (info) => {
+      assume(info.level).is.a('string');
+      assume(info.message).is.a('string');
+      assume(info.level).equals(errInfo.level);
+      assume(info.message).equals(errInfo.message);
+      assume(info[MESSAGE]).equals(errInfo.message);
+    },
+    { immutable: false }
+  ));
+
+  it('errors({ space: 2 }) sets info.stack', assumeFormatted(
+    errors({ stack: true }),
+    errInfo,
+    (info) => {
+      assume(info.level).is.a('string');
+      assume(info.message).is.a('string');
+      assume(info.level).equals(errInfo.level);
+      assume(info.message).equals(errInfo.message);
+      assume(info[MESSAGE]).equals(errInfo.message);
+      assume(info.stack).equals(errInfo.stack);
+    },
+    { immutable: false }
   ));
 });
