a7da3e9e90897c43a22faa4e3f361f83a982d221
indexzero <charlie.robbins@gmail.com>
2019-01-31 10:47:52 -0500

    [fix] Remove all internal symbols before invoking `util.inspect`.

--- test/pretty-print.test.js
+++ test/pretty-print.test.js
@@ -3,19 +3,51 @@
 const util = require('util');
 const assume = require('assume');
 const prettyPrint = require('../pretty-print');
-const helpers = require('./helpers');
-const { MESSAGE } = require('triple-beam');
+const { assumeFormatted, infoify } = require('./helpers');
+const { LEVEL, MESSAGE, SPLAT } = require('triple-beam');
 
 describe('prettyPrint', () => {
-  it('prettyPrint() (default) sets info[MESSAGE]', helpers.assumeFormatted(
+  it('prettyPrint() (default) sets info[MESSAGE]', assumeFormatted(
     prettyPrint(),
-    { level: 'info', message: 'yay template strings are fast' },
+    infoify({ level: 'info', message: 'yay template strings are fast' }),
     (info, expected) => {
       assume(info.level).is.a('string');
       assume(info.message).is.a('string');
       assume(info.level).equals('info');
       assume(info.message).equals('yay template strings are fast');
-      assume(info[MESSAGE]).equals(util.inspect(expected));
+
+      const {
+        [LEVEL]: level, // eslint-disable-line no-unused-vars
+        [MESSAGE]: message, // eslint-disable-line no-unused-vars
+        [SPLAT]: splat, // eslint-disable-line no-unused-vars
+        ...stripped
+      } = info;
+
+      assume(info[MESSAGE]).equals(util.inspect(stripped));
+    }
+  ));
+
+  it('prettyPrint() (default) strips info[SPLAT]', assumeFormatted(
+    prettyPrint(),
+    infoify({
+      level: 'info',
+      message: 'yay template strings are fast',
+      [SPLAT]: [1, 2, 3]
+    }),
+    (info, expected) => {
+      assume(info.level).is.a('string');
+      assume(info.message).is.a('string');
+      assume(info.level).equals('info');
+      assume(info.message).equals('yay template strings are fast');
+
+      const {
+        [LEVEL]: level, // eslint-disable-line no-unused-vars
+        [MESSAGE]: message, // eslint-disable-line no-unused-vars
+        [SPLAT]: splat, // eslint-disable-line no-unused-vars
+        ...stripped
+      } = info;
+
+      assume(info[MESSAGE]).equals(util.inspect(stripped));
     }
   ));
 });
