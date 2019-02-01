d981ec55f313da143c2ca197bb82194fd7d1da44
Charlie Robbins <charlie.robbins@gmail.com>
2019-01-31 19:37:14 -0500

    Remove all internal symbols before invoking `util.inspect`. (#74)
    * [fix] Remove all internal symbols before invoking `util.inspect`.
    
    * [fix] Use correct var.
    
    * [doc] Update README.md
    
    * [fix] Appease the gods of LTS – make node@6 work for a few more months.

--- README.md
+++ README.md
@@ -497,6 +497,9 @@ It accepts the following options:
 
 The `prettyPrint` format should not be used in production because it may impact performance negatively and block the event loop.
 
+> **NOTE:** the `LEVEL`, `MESSAGE`, and `SPLAT` symbols are stripped from the
+> output message _by design._
+
 This was previously exposed as `{ prettyPrint: true }` to transports in `winston < 3.0.0`.
 
 ```js
@@ -505,6 +508,7 @@ const { format } = require('logform');
 const prettyPrintFormat = format.prettyPrint();
 
 const info = prettyPrintFormat.transform({
+  [LEVEL]: 'info',
   level: 'info',
   message: 'my message'
 });
@@ -512,6 +516,7 @@ const info = prettyPrintFormat.transform({
 console.log(info);
 // { level: 'info',
 //   message: 'my message',
+//   [Symbol(level)]: 'info',
 //   [Symbol(message)]: '{ level: \'info\', message: \'my message\' }' }
 ```
 
--- pretty-print.js
+++ pretty-print.js
@@ -2,7 +2,7 @@
 
 const inspect = require('util').inspect;
 const format = require('./format');
-const { LEVEL, MESSAGE } = require('triple-beam');
+const { LEVEL, MESSAGE, SPLAT } = require('triple-beam');
 
 /*
  * function prettyPrint (info)
@@ -11,9 +11,19 @@ const { LEVEL, MESSAGE } = require('triple-beam');
  * { prettyPrint: true } to transports in `winston < 3.0.0`.
  */
 module.exports = format((info, opts = {}) => {
-  // info[LEVEL] is enumerable here, so util.inspect would print it; so it must be manually stripped
-  const strippedInfo = Object.assign({}, info);
-  delete strippedInfo[LEVEL];
-  info[MESSAGE] = inspect(strippedInfo, false, opts.depth || null, opts.colorize);
+  //
+  // info[{LEVEL, MESSAGE, SPLAT}] are enumerable here. Since they
+  // are internal, we remove them before util.inspect so they
+  // are not printed.
+  //
+  const stripped = Object.assign({}, info);
+
+  // Remark (indexzero): update this technique in April 2019
+  // when node@6 is EOL
+  delete stripped[LEVEL];
+  delete stripped[MESSAGE];
+  delete stripped[SPLAT];
+
+  info[MESSAGE] = inspect(stripped, false, opts.depth || null, opts.colorize);
   return info;
 });
