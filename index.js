85d53a691b99d2b6018d03552ffb3ebf83a35459
Yurij Mikhalevich <yurij@mikhalevi.ch>
2021-09-22 06:40:51 +0300

    style: lint autofix

--- index.js
+++ index.js
@@ -33,20 +33,20 @@ function exposeFormat(name, requireFormat) {
 //
 // Setup all transports as lazy-loaded getters.
 //
-exposeFormat('align', function () { return require('./align') });
-exposeFormat('errors', function () { return require('./errors') });
-exposeFormat('cli', function () { return require('./cli') });
-exposeFormat('combine', function () { return require('./combine') });
-exposeFormat('colorize', function () { return require('./colorize') });
-exposeFormat('json', function () { return require('./json') });
-exposeFormat('label', function () { return require('./label') });
-exposeFormat('logstash', function () { return require('./logstash') });
-exposeFormat('metadata', function () { return require('./metadata') });
-exposeFormat('ms', function () { return require('./ms') });
-exposeFormat('padLevels', function () { return require('./pad-levels') });
-exposeFormat('prettyPrint', function () { return require('./pretty-print') });
-exposeFormat('printf', function () { return require('./printf') });
-exposeFormat('simple', function () { return require('./simple') });
-exposeFormat('splat', function () { return require('./splat') });
-exposeFormat('timestamp', function () { return require('./timestamp') });
-exposeFormat('uncolorize', function () { return require('./uncolorize') });
+exposeFormat('align', function () { return require('./align'); });
+exposeFormat('errors', function () { return require('./errors'); });
+exposeFormat('cli', function () { return require('./cli'); });
+exposeFormat('combine', function () { return require('./combine'); });
+exposeFormat('colorize', function () { return require('./colorize'); });
+exposeFormat('json', function () { return require('./json'); });
+exposeFormat('label', function () { return require('./label'); });
+exposeFormat('logstash', function () { return require('./logstash'); });
+exposeFormat('metadata', function () { return require('./metadata'); });
+exposeFormat('ms', function () { return require('./ms'); });
+exposeFormat('padLevels', function () { return require('./pad-levels'); });
+exposeFormat('prettyPrint', function () { return require('./pretty-print'); });
+exposeFormat('printf', function () { return require('./printf'); });
+exposeFormat('simple', function () { return require('./simple'); });
+exposeFormat('splat', function () { return require('./splat'); });
+exposeFormat('timestamp', function () { return require('./timestamp'); });
+exposeFormat('uncolorize', function () { return require('./uncolorize'); });
