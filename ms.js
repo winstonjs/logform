ac9e32dbd09d66794e1c2cca58b86f226482632f
Johnny J. Huirilef Millán <johnnyhuirilef@gmail.com>
2023-12-13 10:14:40 -0300

    perf: update ms to use performance.now() for more accurate time measurements

--- ms.js
+++ ms.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { performance } = require('perf_hooks');
 const format = require('./format');
 const ms = require('ms');
 
@@ -8,11 +9,11 @@ const ms = require('ms');
  * Returns an `info` with a `ms` property. The `ms` property holds the Value
  * of the time difference between two calls in milliseconds.
  */
-module.exports = format(info => {
-  const curr = +new Date();
+module.exports = format((info) => {
+  const curr = performance.now();
   this.diff = curr - (this.prevTime || curr);
   this.prevTime = curr;
-  info.ms = `+${ms(this.diff)}`;
+  info.ms = `+${ms(Math.round(this.diff))}`;
 
   return info;
 });
